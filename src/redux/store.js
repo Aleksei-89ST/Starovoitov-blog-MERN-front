460da776d7e4c90d8d4ad302cc5537d881a5b8d4
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-09-18 21:32:15 +0400

    authReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { postsReducer } from "./slices/posts";
+import { authReducer } from "./slices/auth";
 
 // создание хранилища
 const store = configureStore({
 reducer: {
     posts:postsReducer,
+    auth: authReducer
 }
 })
 
