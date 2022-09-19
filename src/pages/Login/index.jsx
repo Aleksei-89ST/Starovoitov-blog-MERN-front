45284249eb9f32295649d8007f18e56422dcc32e
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-09-19 19:54:00 +0400

    authorization functionality

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,26 +3,62 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 import styles from "./Login.module.scss";
+import { fetchAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+  const dispatch = useDispatch()
+  // вытаскиваю функции из react-hook-form
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    // обьясняю что валидация будет происходить если же эти поля поменялись
+    mode: 'onChange',
+  });
+  // функция котороя будет выполнятся только если react-hook-form понял что валидация прошла успешно
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          // обьясняю что если email рендерится - то сразу их регистрируем в useForm
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          type="password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+                 // обьясняю что если password рендерится - то сразу их регистрируем в useForm
+          {...register("password", { required: "Укажите пароль" })}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
